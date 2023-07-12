30cab9c37dac3069310f0755239b07bc19c2c524
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2023-07-12 20:57:42 +0900

    feat: separate startup as admin and normal

---
+++ CustomStartup.ps1
@@ -0,0 +1,32 @@
+﻿function RunAsync($FilePath,$Arguments,$WinTitle){
+    $command = "Start-Process -FilePath ""$FilePath"""
+    $folderPath = $(Split-Path -Path $FilePath -Parent)
+    if($folderPath -ne ""){$command = $command + " -WorkingDirectory ""$folderPath"""}
+    if($Arguments -ne $null){$command = $command + $Arguments}
+
+    echo $command
+    Invoke-Expression $command
+
+    if($WinTitle -ne $null){
+        While((Get-Process|Where-Object {$_.MainWindowTitle -like $WinTitle}) -eq $null){
+            SLEEP 0.5
+        }
+    }
+}
+
+RunAsync "C:\Program Files\AutoHotkey\Scripts\ScreenLock.ahk"
+RunAsync "C:\Program Files (x86)\PicPick\picpick.exe"
+RunAsync "C:\Program Files\obs-studio\bin\64bit\obs64.exe" -Arguments "--startreplaybuffer --minimize-to-tray"
+RunAsync "explorer.exe"
+RunAsync "C:\Program Files (x86)\Notepad++\notepad++.exe" -WinTitle "* - Notepad++"
+RunAsync "Code" -Arguments ' -ArgumentList "-n -g ""C:\Program Files\AutoHotkey"""' -WinTitle "*AutoHotkey - Visual Studio Code"
+RunAsync "Code" -Arguments ' -ArgumentList "-n -g ""C:\Users\PRO\Documents\Projects\MyChromeExtension"""' -WinTitle "*MyChromeExtension - Visual Studio Code"
+RunAsync "Code" -Arguments ' -ArgumentList "-n -g ""C:\Users\PRO\Documents\Projects\SeleniumProject"""' -WinTitle "*SeleniumProject - Visual Studio Code"
+RunAsync "C:\Users\PRO\Documents\タスク管理.xlsm" -WinTitle "*タスク管理.xlsm*"
+RunAsync "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -WinTitle "* - Google Chrome"
+RunAsync "C:\Program Files\Google\Drive File Stream\launch.bat"
+exit
+
+# メモ
+# -WindowStyle Hidden
+# Get-Process|Where-Object {$_.MainWindowTitle}|Format-Table MainWindowTitle
---
+++ CustomStartupRunas.ps1
@@ -0,0 +1,23 @@
+﻿function RunasAsync($FilePath,$Arguments,$WinTitle){
+    $command = "Start-Process -FilePath ""$FilePath"""
+    $folderPath = $(Split-Path -Path $FilePath -Parent)
+    if($folderPath -ne ""){$command = $command + " -WorkingDirectory ""$folderPath"""}
+    if($Arguments -ne $null){$command = $command + $Arguments}
+
+    echo $command
+    Invoke-Expression $command
+
+     if($WinTitle -ne $null){
+         While((Get-Process|Where-Object {$_.MainWindowTitle -like $WinTitle}) -eq $null){
+             SLEEP 0.5
+        }
+     }
+}
+
+RunasAsync "C:\Program Files\AutoHotkey\AutoHotkey.exe" """""C:\Program Files\AutoHotkey\AutoHotkey.ahk"""""
+RunasAsync "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe"
+RunasAsync "Taskmgr.exe" -WinTitle "タスク マネージャー"
+exit
+
+# メモ
+# Get-Process|Where-Object {$_.MainWindowTitle}|Format-Table MainWindowTitle
