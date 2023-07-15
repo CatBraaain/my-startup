f5caf0864467bf510799c5b726a6a0b798120a06
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2023-07-15 14:52:03 +0900

    fix: prevent showing prompt while vscode open

--- CustomStartup.ps1
+++ CustomStartup.ps1
@@ -19,14 +19,14 @@ RunAsync "C:\Program Files (x86)\PicPick\picpick.exe"
 RunAsync "C:\Program Files\obs-studio\bin\64bit\obs64.exe" ' -ArgumentList "--startreplaybuffer --minimize-to-tray"'
 RunAsync "explorer.exe"
 RunAsync "C:\Program Files (x86)\Notepad++\notepad++.exe" -WinTitle "* - Notepad++"
-RunAsync "Code" ' -ArgumentList "-n -g ""C:\Users\PRO\Documents\Projects\AutoHotkey"""' -WinTitle "*AutoHotkey - Visual Studio Code"
-RunAsync "Code" ' -ArgumentList "-n -g ""C:\Users\PRO\Documents\Projects\MyChromeExtension"""' -WinTitle "*MyChromeExtension - Visual Studio Code"
-RunAsync "Code" ' -ArgumentList "-n -g ""C:\Users\PRO\Documents\Projects\SeleniumProject"""' -WinTitle "*SeleniumProject - Visual Studio Code"
+RunAsync "Code" ' -ArgumentList "C:\Users\PRO\Documents\Projects\AutoHotkey" -WindowStyle Hidden' -WinTitle "*AutoHotkey - Visual Studio Code"
+RunAsync "Code" ' -ArgumentList "C:\Users\PRO\Documents\Projects\MyChromeExtension" -WindowStyle Hidden' -WinTitle "*MyChromeExtension - Visual Studio Code"
+RunAsync "Code" ' -ArgumentList "C:\Users\PRO\Documents\Projects\SeleniumProject" -WindowStyle Hidden' -WinTitle "*SeleniumProject - Visual Studio Code"
 RunAsync "C:\Users\PRO\Documents\タスク管理.xlsm" -WinTitle "*タスク管理.xlsm*"
 RunAsync "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -WinTitle "* - Google Chrome"
 RunAsync "C:\Program Files\Google\Drive File Stream\launch.bat"
+
 exit
 
 # メモ
-# -WindowStyle Hidden
 # Get-Process|Where-Object {$_.MainWindowTitle}|Format-Table MainWindowTitle
