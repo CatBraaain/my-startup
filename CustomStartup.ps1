a57fb12f9c18f694c5e84b74b13885b24a72ee15
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2023-07-29 07:04:18 +0900

    chore: change projects directory

--- CustomStartup.ps1
+++ CustomStartup.ps1
@@ -15,15 +15,16 @@
     }
 }
 
-RunAsync "C:\Users\PRO\Documents\Projects\AutoHotkey\Scripts\ScreenLock.ahk"
+RunAsync "C:\Projects\AutoHotkey\Scripts\ScreenLock.ahk"
 RunAsync "C:\Program Files (x86)\PicPick\picpick.exe"
 RunAsync "C:\Program Files\obs-studio\bin\64bit\obs64.exe" "--startreplaybuffer --minimize-to-tray"
 RunAsync "explorer.exe"
 RunAsync "C:\Program Files (x86)\Notepad++\notepad++.exe" -WinTitle "* - Notepad++"
-RunAsync "Code" "C:\Users\PRO\Documents\Projects\AutoHotkey" -Options "-WindowStyle Hidden" -WinTitle "*AutoHotkey - Visual Studio Code"
-RunAsync "Code" "C:\Users\PRO\Documents\Projects\MyChromeExtension" -Options "-WindowStyle Hidden" -WinTitle "*MyChromeExtension - Visual Studio Code"
-RunAsync "Code" "C:\Users\PRO\Documents\Projects\SeleniumProject" -Options "-WindowStyle Hidden" -WinTitle "*SeleniumProject - Visual Studio Code"
-RunAsync "Code" "C:\Users\PRO\Documents\Projects\MyReminders" -Options "-WindowStyle Hidden" -WinTitle "*MyReminders - Visual Studio Code"
+RunAsync "Code" "C:\Projects\AutoHotkey" -Options "-WindowStyle Hidden" -WinTitle "*AutoHotkey - Visual Studio Code"
+RunAsync "Code" "C:\Projects\MyChromeExtension" -Options "-WindowStyle Hidden" -WinTitle "*MyChromeExtension - Visual Studio Code"
+# RunAsync "Code" "C:\Projects\SeleniumProject" -Options "-WindowStyle Hidden" -WinTitle "*SeleniumProject - Visual Studio Code"
+RunAsync "Code" "C:\Projects\MyReminders" -Options "-WindowStyle Hidden" -WinTitle "*MyReminders - Visual Studio Code"
+# RunAsync "Code" "C:\Projects\GmailFilters" -Options "-WindowStyle Hidden" -WinTitle "*GmailFilters - Visual Studio Code"
 RunAsync "C:\Users\PRO\Documents\タスク管理.xlsm" -WinTitle "*タスク管理.xlsm*"
 RunAsync "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -WinTitle "* - Google Chrome"
 RunAsync "C:\Program Files\Google\Drive File Stream\launch.bat"
--- CustomStartupRunas.ps1
+++ CustomStartupRunas.ps1
@@ -23,7 +23,7 @@ function HideWin($WinTitle){
     $ShowWindowAsync::ShowWindowAsync((Get-Process|Where-Object {$_.MainWindowTitle -like "*$WinTitle*"}).MainWindowHandle, 6)
 }
 
-RunasAsync "C:\Users\PRO\Documents\Projects\AutoHotkey\AutoHotkey.ahk"
+RunasAsync "C:\Projects\AutoHotkey\AutoHotkey.ahk"
 RunasAsync "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe"
 RunasAsync "Taskmgr.exe" -WinTitle "タスク マネージャー"
 HideWin "タスク マネージャー"
