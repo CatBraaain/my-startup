b41c20ee141f21cf24f010e0f0af700e654c4d2d
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-03-31 14:27:42 +0900

    chore: edit startup apps

--- CustomStartup.ps1
+++ CustomStartup.ps1
@@ -7,7 +7,6 @@ RunAsync "C:\Program Files\obs-studio\bin\64bit\obs64.exe" "--startreplaybuffer 
 # RunAsync "C:\Users\PRO\AppData\Local\Programs\Rakuten\Rakuten Link.exe" -WinTitle "Rakuten Link" -CloseWin
 RunAsync "explorer.exe" -WinTitle "PC"
 RunAsync "Code" "C:\Users\PRO\Documents\TasksTodo" -WinTitle "*TasksTodo - Visual Studio Code" -Options "-WindowStyle Hidden"
-RunAsync "Code" "C:\Projects\youtube-automate" -WinTitle "*youtube-automate - Visual Studio Code" -Options "-WindowStyle Hidden"
-RunAsync "Code" "C:\Projects\video-player" -WinTitle "*video-player - Visual Studio Code" -Options "-WindowStyle Hidden"
+RunAsync "Code" "C:\Projects\meigen-py" -WinTitle "*meigen-py - Visual Studio Code" -Options "-WindowStyle Hidden"
 # RunAsync "Code" "C:\Projects\google-colabor" -WinTitle "*google-colabor - Visual Studio Code" -Options "-WindowStyle Hidden"
 RunAsync "C:\Program Files\Google\Chrome\Application\chrome.exe" -WinTitle "* - Google Chrome"
