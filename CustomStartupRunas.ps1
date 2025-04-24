d87fdca5a3b14dfc2088059268094439bc39084d
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-04-24 14:17:08 +0900

    chore: edit startup app's path

--- CustomStartupRunas.ps1
+++ CustomStartupRunas.ps1
@@ -1,6 +1,6 @@
 ﻿using module ".\lib\RunAsync.psm1"
 
-RunAsync "C:\Projects\AutoHotkey\AutoHotkey.ahk"
+RunAsync "C:\Projects\AutoHotkey\src\Main.ahk"
 RunAsync "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe"
 RunAsync "Taskmgr.exe" -WinTitle "タスク マネージャー" -HideWin
 RunAsync "C:\Projects\Windows File History\NetShare.bat"
