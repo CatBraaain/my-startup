f97a7c97c64b1476605e3c332973a01372b79abf
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-05-18 09:11:32 +0900

    feat: avoid prompt when launch

--- CustomStartup.ps1
+++ CustomStartup.ps1
@@ -3,7 +3,7 @@
 RunAsync "C:\Projects\AutoHotkey\Scripts\ScreenLock.ahk"
 RunAsync "C:\Program Files\Google\Drive File Stream\launch.bat"
 RunAsync "C:\Program Files\ShareX\ShareX.exe"
-RunAsync "C:\Program Files\obs-studio\bin\64bit\obs64.exe" "--startreplaybuffer --minimize-to-tray"
+RunAsync "C:\Program Files\obs-studio\bin\64bit\obs64.exe" "--startreplaybuffer --minimize-to-tray --disable-shutdown-check"
 # RunAsync "C:\Users\PRO\AppData\Local\Programs\Rakuten\Rakuten Link.exe" -WinTitle "Rakuten Link" -CloseWin
 RunAsync "explorer.exe" -WinTitle "PC"
 RunAsync "Code" "C:\Users\PRO\Documents\TasksTodo" -WinTitle "*TasksTodo - Visual Studio Code" -Options "-WindowStyle Hidden"
